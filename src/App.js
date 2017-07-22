e50694761ea4ad25f4bc55196092f82ecd4d587f
Nityanand Katare <iknityanand@gmail.com>
2017-07-23 00:05:11 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      input: '/*add our jsc here*/',
+      output:'',
+      err:''
+    }
+  }
+
+  update(e){
+    let code=e.target.value;
+    try{
+      this.setState({
+        output: window.Babel
+        .transform(code, { presets: ['es2015', 'react']})
+        .code,
+        err:''
+      })
+    }catch(err){
+      this.setState({err: err.message})
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Helo world</h1>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea
+          onChange={this.update.bind(this)}
+          defaultValue={this.state.input} />
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
       </div>
     );
   }
